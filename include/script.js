600fd261a27082dfdd6e37d46a1a15b69178dfe9
Ashime Sho <a_2oo+gituseremail@outlook.com>
2024-06-07 21:04:07 -0400

    fancy dropdowns part 2

--- include/script.js
+++ include/script.js
@@ -329,23 +329,40 @@
       // Click the checkbox as we are apart of the checkbox
       const checkbox = target.querySelector('input[type="checkbox"]');
       checkbox.click();
-    } else if (target.nodeName === 'X-DROPDOWN-SPAN') {
+    }
+  });
+  document.querySelectorAll('x-dropdown-span').forEach(span => {
+    // Make sure open and close exist
+    span.dataset.open = span.dataset?.open ?? 'closed';
+    selDropdown(span.dataset.dropdown).dataset.open = span.dataset.open;
+    // Create the SVG arrow
+    // Thanks bootstrap! (https://icons.getbootstrap.com/icons/arrow-up-short/)
+    const dropdownArrowSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    dropdownArrowSvg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    dropdownArrowSvg.setAttribute('width', '16');
+    dropdownArrowSvg.setAttribute('height', '16');
+    dropdownArrowSvg.setAttribute('viewBox', '0 0 16 16');
+    dropdownArrowSvg.setAttribute('class', 'bi bi-arrow-up-short');
+    dropdownArrowSvg.setAttribute('fill', '#ffffff');
+    const dropdownArrowPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    dropdownArrowPath.setAttribute('fill-rule', 'evenodd');
+    dropdownArrowPath.setAttribute('d', 'M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5');
+    dropdownArrowSvg.appendChild(dropdownArrowPath);
+    span.insertBefore(dropdownArrowSvg, span.childNodes[0]);
+    // Add the onclick
+    span.onclick = function() {
       // Make sure "data-dropdown" is defined
-      if (target.dataset?.dropdown) {
+      if (this.dataset?.dropdown) {
         // Get our dropdown content node
-        const dropdownContent = selDropdown(target.dataset.dropdown);
+        const dropdownContent = selDropdown(this.dataset.dropdown);
         // Open / Close it
-        const open = target.dataset.open === 'open' ? 'closed' : 'open'
-        target.dataset.open = open;
+        const open = this.dataset.open === 'open' ? 'closed' : 'open'
+        this.dataset.open = open;
         dropdownContent.dataset.open = open;
         // Redraw
         dropdownContent.offsetHeight;
       }
     }
-  });
-  document.querySelectorAll('x-dropdown-span').forEach(span => {
-    span.dataset.open = span.dataset?.open ?? 'closed';
-    selDropdown(span.dataset.dropdown).dataset.open = span.dataset.open;
   });
 
   // Share some stuff
